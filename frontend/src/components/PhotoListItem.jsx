50aa0e21a892a60a7f524b53d63d2654d9266e6a
Mohamed Waked <mwaked01@gmail.com>
2023-11-12 11:53:52 -0500

    display three photos

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,20 +2,22 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = (props) => {
+  const { sampleDataForPhotoListItem } = props;
+  return (
+    <section className="photo-list__item">
+      <img className="photo-list__image" src={sampleDataForPhotoListItem.imageSource} alt={sampleDataForPhotoListItem.id} />
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={sampleDataForPhotoListItem.profile} alt={sampleDataForPhotoListItem.username} />
+        <footer className="photo-list__user-info">
+          <div>{sampleDataForPhotoListItem.username}</div>
+          <div className="photo-list__user-location">
+            {`${sampleDataForPhotoListItem.location.city}, ${sampleDataForPhotoListItem.location.country}`}
+          </div>
+        </footer>
+      </div>
+    </section>
+  )
 };
 
 export default PhotoListItem;
